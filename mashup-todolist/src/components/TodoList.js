e40e5faf4d79e3d6c56754dae93b0a118b051bc4
Chang-HO Yu <birdegg2@naver.com>
2021-03-19 09:04:57 +0900

    initial commit

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
 import TodoItem from './TodoItem';
 
 
@@ -13,11 +14,17 @@ overflow-y: auto;
 `;
 
 function TodoList() {
+
+    const todos = useTodoState();
+
     return <TodoListBlock>
-       <TodoItem text='프로젝트 생성하기' done={true}></TodoItem>
-       <TodoItem text='컴포넌트 스타일링 하기' done={true}></TodoItem>
-       <TodoItem text='Context  만들기' done={false}></TodoItem>
-       <TodoItem text='기능 구현하기' done={false}></TodoItem>
+         {
+             todos.map(todo=>(
+                 <TodoItem key={todo.id} id={todo.id} text={todo.text} done={todo.done}></TodoItem>
+             ))
+         }
+
+     
     </TodoListBlock>;
     
 }
